b26e538757bdf5f621046c9009d48db2468cb57f
Damian <damianrachwal962gmail.com>
2021-07-14 14:03:07 +0200

    Added text change button

--- testing-react-apps/src/components/Greetings/Greetings.js
+++ testing-react-apps/src/components/Greetings/Greetings.js
@@ -1,5 +1,19 @@
+import { Fragment, useState } from 'react';
+
 const Greetings = () => {
-    return <h1>Hello World!</h1>
+    const [changedText, setChangedText] = useState(false);
+
+    const changedTextHandler = () => {
+        setChangedText(true);
+    };
+
+    return (
+        <Fragment>
+            <h1>Hello World!</h1>
+            {changedText ? <p>Test</p> : <p>Test 2</p>}
+            <button onClick={changedTextHandler}>Change</button>
+        </Fragment>
+    );
 }
 
 export default Greetings;
