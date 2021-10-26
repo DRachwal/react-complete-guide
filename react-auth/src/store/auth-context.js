61a68f379664f0f908fcecbc1dd165c03abcad72
Damian <damianrachwal962gmail.com>
2021-10-26 14:14:14 +0200

    Auto logout modifications after token expiration

--- react-auth/src/store/auth-context.js
+++ react-auth/src/store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect, useCallback } from 'react';
+
+let logoutTimer = null;
 
 const AuthContext = createContext({
     token: null,
@@ -13,22 +15,52 @@ const getRemainingTime = expirationTime => {
     return remainingTime;
 };
 
+const getStoredToken = () => {
+    // Get token & expiration time from local storage
+    const token = localStorage.getItem('token');
+    const expirationTime = localStorage.getItem('expirationTime');
+
+    const remainingTime = getRemainingTime(expirationTime); // Calculate remaining time
+
+    if (remainingTime <= 60000) // If the remaining time is lower or equal then 1 min, return null
+        return null;
+
+    return {
+        token,
+        remainingTime
+    } // In other case return stored token & expiration time
+};
+
 export const AuthContextProvider = ({ children }) => {
-    const initialToken = localStorage.getItem('token');
+    const tokenData = getStoredToken();
+    const initialToken = tokenData ? tokenData.token : null;
+
     const [token, setToken] = useState(initialToken);
 
     const loginHandler = (token, expirationTime) => {
         setToken(token);
         localStorage.setItem('token', token);
+        localStorage.setItem('expirationTime', expirationTime);
 
         const remainingTime = getRemainingTime(expirationTime);
-        setTimeout(logoutHandler, remainingTime); // Logout after token expires
+        logoutTimer = setTimeout(logoutHandler, remainingTime); // Logout after token expires
     };
 
-    const logoutHandler = () => {
+    const logoutHandler = useCallback(() => {
+        // Clear token & expiration time
         setToken(null);
         localStorage.removeItem('token');
-    };
+        localStorage.removeItem('expirationTime');
+
+        if (logoutTimer) // Clear logout timer if user logs out
+            clearTimeout(logoutTimer);
+    }, []);
+
+    useEffect(() => {
+        if (tokenData) {
+            logoutTimer = setTimeout(logoutHandler, tokenData.remainingTime);
+        }
+    }, [tokenData, logoutHandler]);
 
     const contextValue = {
         token,
