c037e4088295c1329120cfbb69c4b245a67a0e9f
Damian <damianrachwal962gmail.com>
2021-11-26 15:51:04 +0100

    Added css tutorial

---
+++ react-css/src/App.tsx
@@ -0,0 +1,13 @@
+import classes from './App.module.css';
+
+const App = () => {
+  return (
+    <main>
+      <section className={classes.section}>
+        <h1>Get the freedom you deserve</h1>
+      </section>
+    </main>
+  );
+}
+
+export default App;
